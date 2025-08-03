e100d712975874b105e0ce12cb55e9482df353bc
Praveen D R <praveendr07@gmail.com>
2025-08-03 21:53:30 +0530

    Update: Created the database and User model

---
+++ src/main/java/com/fullstack/cashPilot/models/User.java
@@ -0,0 +1,9 @@
+package com.fullstack.cashPilot.models;
+
+public class User {
+    private String userId;
+    private String userName;
+    private String email;
+    private String password;
+    private String profileImageUrl;
+}
