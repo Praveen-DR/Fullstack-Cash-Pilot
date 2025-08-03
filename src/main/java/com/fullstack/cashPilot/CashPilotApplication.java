84ed66a3f483c8b8788cfb4c7c118650e12ad126
Praveen D R <praveendr07@gmail.com>
2025-08-03 21:37:10 +0530

    Initial commit

---
+++ src/main/java/com/fullstack/cashPilot/CashPilotApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.cashPilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashPilotApplication.class, args);
+	}
+
+}
